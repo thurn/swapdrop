bb9d4841b0c2c0c39f43dd201f61578a755c8ef4
Derek Thurn <derek@fake.email>
2015-03-15 07:53:58 -0700

    remove TouchKit due to touch recognition problems

--- client/Assets/Scripts/Grid.cs
+++ client/Assets/Scripts/Grid.cs
@@ -3,19 +3,38 @@ using System.Collections;
 
 namespace SwapDrop {
   public class Grid : MonoBehaviour {
-
+ 
+    private const int GridMargin = 4;
+    private const int GridSize = 70;
+    private const int NumberOfSquares = 4;
+ 
     // Use this for initialization
     void Start() {
-      print("Grid position: " + transform.position);
       var screenPosition = Camera.main.WorldToScreenPoint(transform.position);
       print("Grid screen position: " + screenPosition);
       var worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
       print("Grid world position: " + worldPosition);
+      var bounds = renderer.bounds;
+      print("Grid bounds: " + bounds);
+
+      for (int i = 0; i < NumberOfSquares; ++i) {
+        for (int j = 0; j < NumberOfSquares; ++j) {
+          AddTapRecognizerForCell(i, j);
+        }
+      }
+    }
+
+    void Update() {
+      Debug.Log(Input.GetTouch(0).phase);
     }
  
-    // Update is called once per frame
-    void Update() {
-  
+    private void AddTapRecognizerForCell(int column, int row) {
+      // A grid cell at column x and row y has x previous squares before it and
+      // x + 1 margins.
+      var rect = new Rect((column * GridSize) + ((column + 1) * GridMargin),
+                            105 + (row * GridSize) + ((row + 1) * GridMargin),
+                            GridSize,
+                            GridSize);
     }
   }
 }
