856221e972a790b54986bed43de125f11ac0301e
Derek Thurn <derek@fake.email>
2015-03-17 13:20:50 -0700

    set up a controller paradigm.

--- client/Assets/Scripts/UI/Grid.cs
+++ client/Assets/Scripts/UI/Grid.cs
@@ -1,30 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace SwapDrop {
-  public sealed class GridCellTappedEvent {
-    private readonly int _column;
-    private readonly int _row;
- 
-    public int Column { get { return _column; } }
-    public int Row { get { return _row; } }
- 
-    public GridCellTappedEvent(int column, int row) {
-      _column = column;
-      _row = row;
-    }
-
-    public override string ToString() {
-      return string.Format("GridCellTappedEvent [row={0}, column={1}]", _row, _column);
-    }
+namespace SwapDrop.UI {
+  public interface IGrid {
+    void SpawnGemAtCell(GridCell cell, GemType type);
   }
 
-  public class Grid : MonoBehaviour {
- 
+  public class Grid : MonoBehaviour, IGrid {
     private const int GridMargin = 4;
     private const int GridSize = 70;
     private const int NumberOfSquares = 4;
 
+    private readonly GridController _gridController;
+
+    public Grid() {
+      _gridController = new GridController(this);
+    }
+ 
     // Use this for initialization
     void Start() {
       var screenPosition = Camera.main.WorldToScreenPoint(transform.position);
@@ -34,17 +26,21 @@ namespace SwapDrop {
       var bounds = renderer.bounds;
       print("Grid bounds: " + bounds);
     }
-
+ 
     void Update() {
       if (Input.GetMouseButtonUp(0) && renderer.bounds.Contains(Input.mousePosition)) {
-        print("Click " + GetTappedCell(Input.mousePosition));
+        _gridController.OnCellTapped(GetTappedCell(Input.mousePosition));
       }
     }
 
-    private GridCellTappedEvent GetTappedCell(Vector3 tappedPosition) {
+    private GridCell GetTappedCell(Vector3 tappedPosition) {
       Vector3 localPosition = transform.InverseTransformPoint(tappedPosition);
-      return new GridCellTappedEvent((int) localPosition.x / (GridSize + GridMargin),
+      return new GridCell((int) localPosition.x / (GridSize + GridMargin),
           (int) localPosition.y / (GridSize + GridMargin));
+    }
+
+    public void SpawnGemAtCell(GridCell cell, GemType type) {
+      print("Spawn gem at cell: " + cell);
     }
   }
 }
---
+++ client/Assets/Scripts/UI/GridController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SwapDrop.UI {
+  public enum GemType {
+    BLUE,
+    RED
+  }
+
+  public sealed class GridCell {
+    private readonly int _column;
+    private readonly int _row;
+    
+    public int Column { get { return _column; } }
+    public int Row { get { return _row; } }
+    
+    public GridCell(int column, int row) {
+      _column = column;
+      _row = row;
+    }
+    
+    public override string ToString() {
+      return string.Format("GridCell [row={0}, column={1}]", _row, _column);
+    }
+  }
+
+  public class GridController {
+
+    private readonly IGrid _grid;
+
+    public GridController(IGrid grid) {
+      _grid = grid;
+    }
+
+    public void OnCellTapped(GridCell cell) {
+      _grid.SpawnGemAtCell(cell, GemType.BLUE);
+    }
+  }
+}
