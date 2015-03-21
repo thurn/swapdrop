a2c532ee51705410037b04fb98659f6fff7651f2
Derek Thurn <derek@fake.email>
2015-03-20 17:10:18 -0700

    set up test skeleton

---
+++ client/Assets/Editor/Tests/UI/GridTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace SwapDrop.UI {
+  [TestFixture]
+  internal class GridTests {
+    [Test]
+    public void AdditionTest() {
+      Assert.That(5, Is.EqualTo(3 + 2));
+    }
+  }
+}
