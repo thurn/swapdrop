231c9eae061609db9acdae5cb3193ff71b3c5594
Derek Thurn <derek@fake.email>
2015-03-21 17:10:33 -0700

    import StrangeIoC

--- client/Assets/Editor/Tests/UI/GridTests.cs
+++ client/Assets/Editor/Tests/UI/GridTests.cs
@@ -7,6 +7,8 @@ namespace SwapDrop.UI {
   internal class GridTests {
     [Test]
     public void AdditionTest() {
+      GameObject gameObject = new GameObject();
+      gameObject.AddComponent<Grid>();
       Assert.That(5, Is.EqualTo(3 + 2));
     }
   }
