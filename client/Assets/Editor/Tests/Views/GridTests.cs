b0f21543be297eb9c3490b2fa413e5e806ca0e23
Derek Thurn <derek@fake.email>
2015-03-28 11:17:13 -0700

    re-import unit test tools

---
+++ client/Assets/Editor/Tests/Views/GridTests.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+
+namespace SwapDrop.Views {
+  [TestFixture]
+  public class GridTests {
+    [Test]
+    public void TestAddition() {
+      Assert.That(2 + 2, Is.EqualTo(4));
+    }
+  }
+}
