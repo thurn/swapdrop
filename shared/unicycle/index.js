04285cb4f697e06a3df519616b44daf775c9dcb6
Derek Thurn <derek@fake.email>
2015-05-04 11:31:40 -0700

    working on unicycle

--- shared/unicycle/index.js
+++ shared/unicycle/index.js
@@ -2,6 +2,8 @@
 
 var through = require('through2');
 var rs = require('replacestream');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
 
 module.exports = function(search, replacement, options) {
   var doReplace = function(file, enc, callback) {
@@ -10,13 +12,18 @@ module.exports = function(search, replacement, options) {
     }
 
     function doReplace() {
-      file.contents = file.contents.pipe(rs(search, replacement));
-      return callback(null, file);
+      if (file.isStream()) {
+        this.emit('error', new PluginError("gulp-unicycle",
+            "Streams are not supported!"));
+      }
+
+      if (file.isBuffer()) {
+        file.contents = new Buffer(String(file.contents).replace(search, replacement));
+        return callback(null, file);
+      }
       callback(null, file);
     }
-
     doReplace();
   };
-
   return through.obj(doReplace);
 };
